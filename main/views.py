ea6921a46a2395220debfa0e18d63e1a3d6e8787
Eversus <yagrigorenkoac@gmail.com>
2022-11-01 18:29:43 +0300

    stable ver1.2, with templates

--- main/views.py
+++ main/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 
 def index(request):
     return render(request, 'main/index.html')
 
 def about(request):
-    return HttpResponse('<h4>О Нас</h4>')
+    return render(request, 'main/about.html')
